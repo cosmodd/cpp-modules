8435f450f6606762e00d8aa80b0156081a88b5e9
mrattez <mrattez@student.42nice.fr>
2022-07-13 16:43:40 +0200

    🔄 CPP02/ex02 progress

---
+++ CPP02/ex02/srcs/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrattez <mrattez@student.42nice.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 12:42:55 by mrattez           #+#    #+#             */
+/*   Updated: 2022/07/13 16:27:15 by mrattez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+
+	std::cout << (Fixed(1) / Fixed(4)) << std::endl;
+	// std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}
